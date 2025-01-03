e077a095212177090fdcfb82641e2c966919f76f
dezao <acoliveira87@gmail.com>
2025-01-03 16:00:20 -0300

    feat: add custom colors to tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-blue-500 font-graphik-bold text-center mt-10">
+      <h1 className="text-primary bg-dark-brackground font-graphik-bold">
         Hello World!
       </h1>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       fontFamily: {
         graphik: ['Graphik', 'sans-serif'],
         'graphik-bold': ['Graphik-Bold', 'sans-serif'],
+      },
+      colors: {
+        primary: '#2091F9',
+        text: '#252B42',
+        'secondary-text': '#374754',
+        'dark-brackground': '#252B42'
       }
     },
   },
